0f58620b6972fa3e09ced54ef89b9cc0b0e25b20
Bibarius <45160369+Bibarius@users.noreply.github.com>
2019-03-08 23:38:43 +0700

    some stuff

--- kernel/build.bat
+++ kernel/build.bat
@@ -2,5 +2,6 @@
 gcc -ffreestanding -m32 -c kernel.c -o kernel.o
 ld -m i386pe -Ttext 0x1000 -o file.tmp kernel.o
 objcopy -O binary file.tmp kernel.bin
+dd seek=1 if=kernel.bin of=my.img 
 
 PAUSE
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,4 +1,4 @@
-void init()
+void start()
 {
     char* video_memory = (char*) 0xb8000;
     *video_memory = 'X';
